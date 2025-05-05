f7329deddc7c77349d3fe1589373b7c1a6783c0b
Ricardo Patino <rpatino12@outlook.com>
2025-05-05 17:17:28 -0500

    Expose a REST API secured by Spring Security using HTTP Basic authentication and in memory users

---
+++ src/main/java/dev/rpatino12/jwt_demo/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package dev.rpatino12.jwt_demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                // To avoid csrf attack, never disable csrf protection while leaving session management enabled (line 24)
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(requests -> requests
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+        return new InMemoryUserDetailsManager(
+                User.withUsername("ricardo")
+                        .password("{noop}password")
+                        .authorities("read")
+                        .build()
+        );
+    }
+}
---
+++ src/main/java/dev/rpatino12/jwt_demo/controller/HomeController.java
@@ -0,0 +1,15 @@
+package dev.rpatino12.jwt_demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+public class HomeController {
+
+    @GetMapping
+    public String home(Principal principal){
+        return String.format("Hello, %s", principal.getName());
+    }
+}
